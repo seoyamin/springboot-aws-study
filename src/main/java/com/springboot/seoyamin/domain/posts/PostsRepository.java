63edc820e266154d726ea653033066d10a15ad92
seoyamin <seoyamin@ewhain.net>
2022-08-31 23:53:20 +0900

    feat : Posts, PostsRepositoryTest

---
+++ src/main/java/com/springboot/seoyamin/domain/posts/PostsRepository.java
@@ -0,0 +1,6 @@
+package com.springboot.seoyamin.domain.posts;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PostsRepository extends JpaRepository<Posts, Long> {  // <Entity 클래스, PK 타입>
+}
